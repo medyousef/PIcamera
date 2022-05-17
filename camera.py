358fc192add5c101eb12a54c0ca9406ae71647c8
MohamedYoussef <medyousef95@gmail.com>
2022-05-17 12:49:30 +0200

    fix pics

--- camera.py
+++ camera.py
@@ -4,7 +4,15 @@ camera=PiCamera()
 camera.resolution = (1280,720)
 camera.rotation = 180
 time.sleep(2)
-file_name="image.jpg"
-camera.capture(file_name)
-print("Done. ")
+file_name_1="image.jpg"
+camera.capture(file_name_1)
+print("pic taken")
+time.sleep(1)
+file_name_2="vid.h264"
+camera.start_recording(file_name_2)
+camera.wait_recording(5)
+camera.stop_recording()
+
+print("vid taken")
+
 #comment
