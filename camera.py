3e493ddcaab1076e0c67ee8c80228d36218d32de
MohamedYoussef <medyousef95@gmail.com>
2022-05-17 12:44:21 +0200

    fix pics

--- camera.py
+++ camera.py
@@ -1,9 +1,10 @@
-from picamera import PIcamera
+from picamera import PiCamera
 import time
-camera=PIcamera()
+camera=PiCamera()
 camera.resolution = (1280,720)
 camera.rotation = 180
 time.sleep(2)
 file_name="/home/pi/Desktop/camera/image.jpg"
 camera.capture(file_name)
 print("Done. ")
+#comment
