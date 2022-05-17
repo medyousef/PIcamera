81ef2d055d4a603c0fd2609e08c35f5a53df11e6
MohamedYoussef <medyousef95@gmail.com>
2022-05-17 13:03:44 +0200

    pic 5sec

--- camera.py
+++ camera.py
@@ -1,10 +1,25 @@
+from turtle import home
 from picamera import PiCamera
 import time
+import os
+import RPi.GPIO as GPIO
+os.mkdir(home/pi/Desktop/PIcamera/pictures)
 camera=PiCamera()
 camera.resolution = (1280,720)
 camera.rotation = 180
+suff=0
 time.sleep(2)
-file_name_1="image.jpg"
+try :
+    while True:
+        file_name="image"+suff+".jpg"
+        camera.capture(file_name)
+        suff+=1
+        time.sleep(5)
+except KeyboardInterrupt:
+    GPIO.cleanup
+        
+
+""" file_name_1="image.jpg"
 camera.capture(file_name_1)
 print("pic taken")
 time.sleep(1)
@@ -14,5 +29,5 @@ camera.wait_recording(5)
 camera.stop_recording()
 
 print("vid taken")
-
+ """
 #comment
